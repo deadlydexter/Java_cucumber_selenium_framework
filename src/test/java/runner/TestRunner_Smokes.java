1345efb09a3067aee3c37ff7fbf189df547ce374
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2021-10-25 20:30:27 -0400

    Commit all files

--- src/test/java/runner/TestRunner_Smokes.java
+++ src/test/java/runner/TestRunner_Smokes.java
@@ -1,29 +1,27 @@
 package runner;
 
-import com.cucumber.listener.Reporter;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import managers.FileReaderManager;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/functionalTests",
+		features = "src/test/resources",
 		glue = "stepDefinitions",
-		tags = "@SmokeTest",
+		tags = "@UI",
 		monochrome = true,
-		plugin =  { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"} )
+		plugin =  { "html:target/cucumber-html-report/report.html"},publish = true )
 public class TestRunner_Smokes {
 
 	@AfterClass
 	public static void writeExtentReport() {
-		Reporter.loadXMLConfig(FileReaderManager.getInstance().getConfigFileReader().getReportConfigPath());
-		Reporter.setSystemInfo("User name", System.getProperty("user.name"));
-		Reporter.setSystemInfo("TimeZone", System.getProperty("user.timeZone"));
-		Reporter.setSystemInfo("Machine", 	"Windows 10" + "64 Bit");
-		Reporter.setSystemInfo("Selenium", "3.7.0");
-		Reporter.setSystemInfo("Maven", "3.5.3");
-		Reporter.setSystemInfo("Java Version", "1.8.0_151");
+//		Reporter.loadXMLConfig(FileReaderManager.getInstance().getConfigFileReader().getReportConfigPath());
+//		Reporter.setSystemInfo("User name", System.getProperty("user.name"));
+//		Reporter.setSystemInfo("TimeZone", System.getProperty("user.timeZone"));
+//		Reporter.setSystemInfo("Machine", 	"Windows 10" + "64 Bit");
+//		Reporter.setSystemInfo("Selenium", "3.7.0");
+//		Reporter.setSystemInfo("Maven", "3.5.3");
+//		Reporter.setSystemInfo("Java Version", "1.8.0_151");
 	}
 }
